48f8c14b136c200c5cfeba80e635fcdd74868b52
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-07-30 09:03:57 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === '') {
+      return;
+    }
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Lovely Todo List</h1>
+      <div className="todo-input">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={handleInputChange}
+          placeholder="Enter a new todo"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      {todos.length === 0 ? (
+        <p className="no-todos-message">No todos yet. Add some tasks!</p>
+      ) : (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <li key={index} className="todo-item">
+              <span>{todo}</span>
+              <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
